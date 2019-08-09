6b500abe859af676758824badc9b3a310e9d701e
James Bracy <waratuman@gmail.com>
2019-08-09 12:32:27 -0700

    Defaulting metadata to an empty hash.

--- lib/activehistory/event.rb
+++ lib/activehistory/event.rb
@@ -24,6 +24,7 @@ class ActiveHistory::Event
 
     @actions ||= []
     @timestamp ||= Time.now
+    @metadata ||= {}
   end
 
   def persisted?
--- lib/activehistory/version.rb
+++ lib/activehistory/version.rb
@@ -1,3 +1,3 @@
 module ActiveHistory
-  VERSION = '6.0.0.1'
+  VERSION = '6.0.0.2'
 end
